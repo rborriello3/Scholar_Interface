065107baef89d7448d90a2ac9b3a1c4e3b6ebe0f
Ray Borriello <rborriello2@gmail.com>
2016-11-07 12:43:28 -0500

    Began model/Event.

---
+++ app/models/Event.php
@@ -0,0 +1,15 @@
+<?php
+
+class Event extends Eloquent
+{
+    /**
+     * The database table
+     */
+    protected $table = 'event';
+
+    /**
+     * We don't want any default time stamps
+     */
+    public $timestamps = FALSE;
+
+    
