d9ad883ea9cf1c6fe056103cdecc1a391c92594c
Ray Borriello <rborriello2@gmail.com>
2017-01-30 13:47:22 -0500

    Started showEditMeeting.

--- app/routes/Authenticated/Admin/Meeting.php
+++ app/routes/Authenticated/Admin/Meeting.php
@@ -4,3 +4,9 @@ Route::get('new', array('uses' => 'MeetingController@showCreateMeeting', 'as' =>
 Route::post('new', array('uses' => 'MeetingController@doCreateMeeting', 'as' => 'doCreateMeeting'));
 Route::get('deactivate/{meetingID}', array('uses' => 'MeetingController@deactivateMeeting', 'as' => 'deactivateMeeting', 'before' => 'meetingID'));
 Route::get('activate/{meetingID}', array('uses' => 'MeetingController@activateMeeting', 'as' => 'activateMeeting', 'before' => 'meetingID'));
+
+Route::group(array('before' => 'meetingID'), function()
+{
+    Route::get('edit/{meetingID}', array('uses' => 'MeetingController@showEditMeeting', 'as' => 'showEditMeeting'));
+    Route::post('edit/{meetingID}', array('uses' => 'MeetingController@doEditMeeting', 'as' => 'doEditMeeting'));
+});
