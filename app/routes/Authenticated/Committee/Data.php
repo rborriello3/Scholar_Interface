1f72f599d042c1ca8158495c1fde6719d8f7f588
Ray Borriello <rborriello2@gmail.com>
2017-02-06 13:00:05 -0500

    Committee Dashboard view started. Meetings and Deadlines tab displaying correctly.

--- app/routes/Authenticated/Committee/Data.php
+++ app/routes/Authenticated/Committee/Data.php
@@ -4,3 +4,5 @@ Route::get('scoring', array(
 	'uses' => 'ApplicationsDataController@gradingApplications',
 	'as'   => 'allGradingApplicationsJson'	
 ));
+Route::get('data/meeting', array('uses' => 'MeetingDataController@showCommMemberMeetings', 'as' => 'showCommMemberMeetings'));
+Route::get('data/deadline', array('uses' => 'DeadlineDataController@showCommMemberDeadlines', 'as' => 'showCommMemberDeadlines'));
--- app/views/Content/Global/Dashboard/dashboard4.blade.php
+++ app/views/Content/Global/Dashboard/dashboard4.blade.php
@@ -3,8 +3,56 @@
 @section('head')
 <title>Scholarship Interface Committee Dashboard</title>
 @parent
+<script type="text/javascript" src="{{asset('/jqueryUI/js/jquery-ui-1.10.3.custom.min.js')}}"></script>
+<script type="text/javascript" src="{{asset('/javascript/Admin/Meeting/viewMeetings.js')}}"></script>
+<script type="text/javascript" src="{{asset('DataTables-1.9.4/media/js/jquery.dataTables.min.js')}}"></script>
+<link rel="stylesheet" type="text/css" href="{{asset('/jqueryUI/css/ui-darkness/jquery-ui-1.10.3.custom.min.css')}}">
+<link rel="stylesheet" type="text/css" href="{{asset('DataTables-1.9.4/media/css/demo_table_jui.css') }}">
+<link rel="stylesheet" type="text/css" href="{{asset('/css/Admin/Meeting/viewMeetings.css')}}">
+
 @stop
 
 @section('dashBoardContent')
+<div class="dashboardMenu">
+    <h3>Meetings</h3>
+    <div class="container">
+   	{{ Datatable::table()
+	->setURL(route('showCommMemberMeetings'))
+	->addColumn('Actions', 'Date', 'Time', 'Place', 'Grade Group/Person')
+	->setOptions('bProcessing', true)
+	->setOptions('bSort', false)
+	->setOptions('iDisplayLength', 5)
+	->setOptions('aLengthMenu', [5, 10, 15, 20, 25, 30, 35, 40, 45, 50])
+	->setOptions('bAutoWidth', false)
+	->setOptions('aoColumns', array(
+	    array('sWidth' => '1%'),
+	    array('sWidth' => '1%'),
+	    array('sWidth' => '1%'),
+	    array('sWidth' => '5%'),
+  	    array('sWidth' => '20%')
+	))
+	->render() }}	        
+    </div>
+</div>
 
+<div class="dashboardMenu">
+    <h3>Deadlines</h3>
+    <div class="container">
+        {{ Datatable::table()
+	->setURL(route('showCommMemberDeadlines'))
+	->addColumn('Actions', 'Date', 'Description', 'Grade Group')
+	->setOptions('bProcessing', true)
+	->setOptions('bSort', false)
+	->setOptions('iDisplayLength', 5)
+	->setOptions('aLengthMenu', [5, 10, 15, 20, 25, 30, 35, 40, 45, 50])
+	->setOptions('bAutoWidth', false)
+	->setOptions('aoColumns', array(
+	    array('sWidth' => '1%'),
+	    array('sWidth' => '1%'),
+	    array('sWidth' => '1%'),
+	    array('sWidth' => '20%')
+	))
+	->render() }}
+    </div>
+</div>
 @stop
